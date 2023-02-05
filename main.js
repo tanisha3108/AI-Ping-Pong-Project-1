73741b0a505d270dca20aaade03fa1bdfa081451
Tanisha Kumar <84838665+tanisha3108@users.noreply.github.com>
2023-02-05 17:18:17 +0800

    Add files via upload

--- main.js
+++ main.js
@@ -21,10 +21,26 @@ var ball = {
     dy:3
 }
 
-function setup(){
-  var canvas =  createCanvas(700,600);
+function preload() {
+  ball_touch_paddel = loadSound("ball_touch_paddel.wav");
+  missed = loadSound("missed.wav");
 }
 
+function setup(){
+var canvas =  createCanvas(700,600);
+canvas.parent('canvas');
+
+video = createCapture(VIDEO);
+video.size(700, 600);
+video.hide();
+
+poseNet = ml5.poseNet(video, modelLoaded);
+poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded() {
+  console.log('PoseNet Is Initialized');
+}
 
 function draw(){
 
